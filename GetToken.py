5e4150f81eefd7ada22633834cfecec39eebb3f9
Shravan1799 <69885425+shravan1799@users.noreply.github.com>
2022-09-30 14:15:02 +0530

    minor changes

--- GetToken.py
+++ GetToken.py
@@ -3,10 +3,10 @@ import base64
 
 
 class GetToken:
-    def __init__(self, id, secret, token_url):
+    def __init__(self, id, secret):
         self.id = id
         self.secret = secret
-        self.token_url = token_url
+        self.token_url = "https://accounts.spotify.com/api/token"
 
     def create_token_header(self, id, secret):
         client_cr = f"{id}:{secret}"
@@ -17,11 +17,16 @@ class GetToken:
         return header
 
     def request_token(self, token_url, data, header):
-        res = requests.post(token_url, data=data, headers=header)
-        res_json = res.json()
-        return res_json["access_token"]
+        try:
+            res = requests.post(token_url, data=data, headers=header)
+            res_json = res.json()
+            return res_json["access_token"]
+        except Exception as e:
+            return None
 
     def start(self):
         ct = self.create_token_header(id=self.id, secret=self.secret)
         rt = self.request_token(token_url=self.token_url, data={"grant_type": "client_credentials"}, header=ct)
+        if rt is None:
+            return None
         return rt
--- main.py
+++ main.py
@@ -4,11 +4,13 @@ from GetSongs import GetSongs
 
 id = "xxx"
 secret = "xxx"
-token_url = "https://accounts.spotify.com/api/token"
 playlist_url = input("Playlist Link: ")
-gt = GetToken(id, secret, token_url)
+gt = GetToken(id, secret)
 token = gt.start()
-print(token)
-gs = GetSongs(token, playlist_url)
-all_songs = gs.start_get_songs()
-print(all_songs)
+if token is None:
+    print("Unauthorised, make sure you save the playlist to your library")
+else:
+    print(token)
+    gs = GetSongs(token, playlist_url)
+    all_songs = gs.start_get_songs()
+    print(all_songs)
