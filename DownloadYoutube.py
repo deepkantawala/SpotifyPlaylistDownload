0859eb5d7e13217ef362823703e882daa0d5c491
Shravan1799 <69885425+shravan1799@users.noreply.github.com>
2022-10-17 15:02:20 +0530

    minor changes

--- DownloadYoutube.py
+++ DownloadYoutube.py
@@ -4,20 +4,20 @@ import pytube as pt
 class GetYTSongs:
 
     def pytube_obj_to_dict(self, obj):
-        '''
-        This method will convery pytube object to dict
+        """
+        This method will convey pytube object to dict
         :param obj: pytube search object
         :return:
-        '''
+        """
         dict_obj = obj.__dict__
         return dict_obj['watch_url']
 
     def make_songs_query(self, res):
-        '''
+        """
         This method makes a query that will be used to get url from youtube with the help of pytube
         :param res: result from spotify api with song name and artist name
         :return:
-        '''
+        """
         temp_dict = {}
         for i in range(0, len(res)):
             b = res[i]
@@ -29,12 +29,12 @@ class GetYTSongs:
         return temp_dict
 
     def map_songs_to_url(self, songs_query):
-        '''
+        """
         this method uses pytube to get first two url's of a youtube search query and returns a dict with song's name,
         first url, and alt url.
         :param songs_query:
         :return: returns a dict with song name, and two youtube url of that song.
-        '''
+        """
         temp_ulr_dict = {}
         search_error_songs = {}
         for i in range(0, len(songs_query)):
@@ -46,7 +46,6 @@ class GetYTSongs:
                 alt_url = self.pytube_obj_to_dict(search_res[1])
                 temp_ulr_dict[i] = {"name": songs_query[i], "first_url": url, "alt_url": alt_url}
                 print(f"{i} out of {len(songs_query)} done!")
-
             except:
                 print(f"Error searching {songs_query[i]}")
                 search_error_songs[i] = songs_query[i]
@@ -55,19 +54,19 @@ class GetYTSongs:
         return temp_ulr_dict
 
     def download_song(self, url_dict):
-        '''
+        """
         this method is used to download songs from youtube
         :param url_dict: a dict with song name, youtube url,and alternative url for the same song
         :return:
-        '''
+        """
         print("URL dict is: ", url_dict)
-        for i in range(0,len(url_dict)):
+        for i in range(0, len(url_dict)):
             try:
                 try:
                     yt = pt.YouTube(url=url_dict[i]['first_url'])
                     yt.streams.filter(file_extension="mp3")
                     a = yt.streams.get_highest_resolution().download(filename=f"C:/songs/{url_dict[i]['name']}.mp3")
-                    print(url_dict[i]['name']," Downloaded successfully!!")
+                    print(url_dict[i]['name'], " Downloaded successfully!!")
                 except:
                     yt = pt.YouTube(url=url_dict[i]['alt_url'])
                     yt.streams.filter(file_extension="mp3")
@@ -76,18 +75,15 @@ class GetYTSongs:
             except:
                 print(f"error while downloading {url_dict[i]['name']}")
 
-
-
     def start(self, spotify_dict):
-        '''
+        """
         This is a start method for the class GetYTSongs which will call other methods in order to get the songs you want to download.
         :param spotify_dict: A dict that you get from spotify's api as response; it should contain data.
         :return:
-        '''
+        """
         print("Executing make_songs_query")
         query = self.make_songs_query(spotify_dict)
         print("Executing map_songs_to_url")
         url_query = self.map_songs_to_url(query)
         print("executing download_song")
         final_download = self.download_song(url_query)
-
--- README.md
+++ README.md
@@ -1,22 +1,22 @@
-# I often have to travel through bus to get to my college, and my part-time job which is quite far and i like to listen to music when I travel; and
-# there are few place on the way where i get network issue, I also have to save my internet data as it is limited so i came up with a idea and 
-# created a project named SpotifyPlaylistDownload with my friend Shravan Patel, which will download songs from spotify playlist after which i cna listen to songs without using internet data  
+# I often have to travel through bus to get to my college, and my part-time job which is quite far and i like to listen to music when I travel.
+# There are quite a few places on the way where I get network issue, I also have to save my internet data as it is limited so I came up with a idea and 
+# Created a project named SpotifyPlaylistDownload with my friend Shravan Patel, which will download songs from spotify playlist after which i can listen to songs without using internet data  
 
 ## This project contains the following files:
 
 ### - DownloadYoutube.py : This file is used to download songs from youtube.
-### - GetPlaylists.py : This file is used to get spotify playlist of a user.
+### - GetPlaylists.py : This file is used to get spotify playlist of a user. (under Development)
 ### - GetSongs.py : This file is used to get playlisy through spotify API
 ### - GetToken.py : This file is used to get Bearer token which is required to get playlist from spotify's API.
 ### - main.py : This is a main file which we execute and it will call methods from other files in order to download the songs
 
 ## Known issues:
 
-### - [ ] pytube's search object sometimes throws execption for few songs.
-### - [ ] Some songs which are more popular than the original one are downloaded instead.
+### - [1] pytube's search object sometimes throws execption for few songs.
+### - [2] Some songs which are more popular than the original one are downloaded instead.
 
 ## Future plan:
 
-### - [x] Bypass age restriction for some explicit songs.
-### - [ ] Better logging.
-### - [ ] Check previously downloaded songs and ignore them while downloading.
+### - [1] Bypass age restriction for some explicit songs.
+### - [2] Better logging.
+### - [3] Check previously downloaded songs and ignore them while downloading.
